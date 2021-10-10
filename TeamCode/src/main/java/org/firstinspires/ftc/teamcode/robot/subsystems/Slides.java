817a1effac328841b5cbf6fb82167a4be3f0fd36
Blake Sanders <54512462+BenRodkin@users.noreply.github.com>
2021-10-10 17:48:58 -0400

    Made Slide subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Slides.java
@@ -0,0 +1,103 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+
+import com.disnodeteam.dogecommander.Subsystem;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+import androidx.annotation.NonNull;
+
+public class Slides implements Subsystem {
+    private HardwareMap hardwareMap;
+
+    private DcMotorEx slide;
+
+    public enum TARGETS {
+        IN,
+        OUT;
+
+        @NonNull
+        public int getTargets() {
+            switch (this) {
+                case IN:
+                    return 0;
+                case OUT:
+                    return 100;
+                default:
+                    return 0;
+            }
+        }
+    }
+
+    private double power = 0.0;
+
+    private int targetPos;
+
+    public Slides(HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+
+        initHardware();
+    }
+
+    public void initHardware() {
+        slide = hardwareMap.get(DcMotorEx.class, "slide");
+        targetPos = slide.getCurrentPosition();
+
+        slide.setTargetPosition(targetPos);
+        slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        slide.setPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION,
+                new PIDFCoefficients(5.0, 0, 0, 0));
+    }
+
+    @Override
+    public void periodic() {
+        if (slide.getMode() == DcMotor.RunMode.RUN_TO_POSITION) {
+            slide.setTargetPosition(targetPos);
+        }
+
+        slide.setPower(power);
+    }
+
+
+    public DcMotor.RunMode getRunMode() {
+        return slide.getMode();
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public boolean isBusy() {
+        return slide.isBusy();
+    }
+
+    public void setRunMode(DcMotor.RunMode runMode) {
+        slide.setMode(runMode);
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    public void setTargetPos(int targetPos) {
+        this.targetPos = targetPos;
+    }
+
+    public int getCurrentPos() {
+        return slide.getCurrentPosition();
+    }
+
+    public int getTargetPos() {
+        return slide.getTargetPosition();
+    }
+
+    public void resetEncoder() {
+        DcMotor.RunMode currentRunMode = slide.getMode();
+        slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slide.setMode(currentRunMode);
+    }
+}
