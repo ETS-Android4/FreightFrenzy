25286506edd1addc67ccca39f6ab81e97dfe2806
Blake Sanders <54512462+BenRodkin@users.noreply.github.com>
2021-10-10 17:48:43 -0400

    Made test program for Arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestArm.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.disnodeteam.dogecommander.DogeCommander;
+import com.disnodeteam.dogecommander.DogeOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpArmControl;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
+
+public class TestArm extends LinearOpMode implements DogeOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DogeCommander commander = new DogeCommander(this);
+
+        Arm arm = new Arm(hardwareMap);
+
+        commander.registerSubsystem(arm);
+
+        commander.init();
+
+        waitForStart();
+
+        commander.runCommandsParallel(new TeleOpArmControl(arm, gamepad1, telemetry));
+    }
+}
