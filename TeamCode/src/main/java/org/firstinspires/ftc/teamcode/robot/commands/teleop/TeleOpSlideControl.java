df7458ab1f93b7b245dc9c40c7a45ae6720ae4ff
Blake Sanders <54512462+BenRodkin@users.noreply.github.com>
2021-10-10 17:49:14 -0400

    Made Slide controls for TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/teleop/TeleOpSlideControl.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode.robot.commands.teleop;
+
+import com.disnodeteam.dogecommander.Command;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Slides;
+
+public class TeleOpSlideControl implements Command {
+    private Gamepad gamepad;
+    private Slides slides;
+    private Telemetry telemetry;
+
+    private final double POWER_SCALAR = 0.4;
+    final double THRESHOLD = 0.5;
+
+    public TeleOpSlideControl(Slides slides, Gamepad gamepad, Telemetry telemetry) {
+        this.slides = slides;
+        this.gamepad = gamepad;
+        this.telemetry = telemetry;
+    }
+
+    public TeleOpSlideControl(Slides slides, Gamepad gamepad) {
+        this(slides, gamepad, null);
+    }
+
+    @Override
+    public void start() {
+        slides.setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slides.setPower(0.0);
+    }
+
+    @Override
+    public void periodic() {
+        double powerIn      = gamepad.left_trigger;
+        double powerOut     = gamepad.right_trigger;
+        boolean encoderIn   = gamepad.left_bumper;
+        boolean encoderOut  = gamepad.right_bumper;
+
+        if(powerIn > THRESHOLD || powerOut > THRESHOLD) {
+            slides.setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            powerIn = powerIn > THRESHOLD ? 1 : 0;
+            powerOut = powerOut > THRESHOLD ? 1 : 0;
+            slides.setPower((powerIn - powerOut) * POWER_SCALAR);
+        } else if(encoderIn ^ encoderOut) {
+            slides.setRunMode(DcMotor.RunMode.RUN_TO_POSITION);
+            slides.setPower(POWER_SCALAR);
+            if(encoderIn) {
+                slides.setTargetPos(Slides.TARGETS.IN.getTargets());
+            }
+            if(encoderOut) {
+                slides.setTargetPos(Slides.TARGETS.OUT.getTargets());
+            }
+        }
+
+        telemetry.addLine("Slide Telemetry:");
+        telemetry.addData("PowerIn:", powerIn);
+        telemetry.addData("PowerOut:",powerOut);
+        telemetry.addData("EncoderIn:", encoderIn);
+        telemetry.addData("EncoderOut:",encoderOut);
+        telemetry.addLine();
+        telemetry.addData("Power:", slides.getPower());
+        telemetry.addData("Current Pos:", slides.getCurrentPos());
+        telemetry.addData("Target Pos:", slides.getTargetPos());
+        telemetry.addData("RunMode", slides.getRunMode());
+        telemetry.update();
+
+    }
+
+    @Override
+    public void stop() {
+        slides.setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slides.setTargetPos(slides.getCurrentPos());
+        slides.setPower(0);
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return false;
+    }
+}
