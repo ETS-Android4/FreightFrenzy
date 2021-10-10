854b5989dc95d9fee45aae44b8eb42d7ed5ae520
Blake Sanders <54512462+BenRodkin@users.noreply.github.com>
2021-10-10 17:47:55 -0400

    Make test program for Camera

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestCamera.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.disnodeteam.dogecommander.DogeCommander;
+import com.disnodeteam.dogecommander.DogeOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.checkerframework.checker.units.qual.C;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.AutoCameraControl;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Camera;
+
+public class TestCamera extends LinearOpMode implements DogeOpMode {
+    private AutoCameraControl cam;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DogeCommander commander = new DogeCommander(this);
+
+        cam = new AutoCameraControl(new Camera(hardwareMap), gamepad1, gamepad2, telemetry);
+
+        while (opModeIsActive() && !isStopRequested()) {
+            cam.periodic();
+        }
+
+        commander.init();
+        waitForStart();
+
+    }
+}
