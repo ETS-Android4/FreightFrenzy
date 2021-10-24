d1ad2d72927de297c0ef833ff25f0462a0f71cb6
Blake Sanders <54512462+BenRodkin@users.noreply.github.com>
2021-10-24 15:40:50 -0400

    Put all TeleOp Commands together for main TeleOp Mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandDrive.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.app.slice.Slice;
+
+import com.disnodeteam.dogecommander.DogeCommander;
+import com.disnodeteam.dogecommander.DogeOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpArmControl;
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpCarouselControl;
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpDriveControl;
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpFlapControl;
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpIntakeControl;
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpSlideControl;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Carousel;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Flap;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Slides;
+
+public class CommandDrive extends LinearOpMode implements DogeOpMode {
+    public static final float TRIGGER_THRESHOLD = 0.7f;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DogeCommander commander = new DogeCommander(this);
+
+        Arm arm = new Arm(hardwareMap);
+        Drive drive = new Drive(hardwareMap);
+        Flap flap = new Flap(hardwareMap);
+        Slides slides = new Slides(hardwareMap);
+        Carousel carousel = new Carousel(hardwareMap);
+        Intake intake = new Intake(hardwareMap);
+
+        commander.registerSubsystem(arm);
+        commander.registerSubsystem(drive);
+        commander.registerSubsystem(flap);
+        commander.registerSubsystem(slides);
+        commander.registerSubsystem(carousel);
+        commander.registerSubsystem(intake);
+
+        commander.init();
+        waitForStart();
+
+        commander.runCommandsParallel(
+                new TeleOpArmControl(arm, gamepad1),
+                new TeleOpDriveControl(drive, gamepad1),
+                new TeleOpFlapControl(flap, gamepad1),
+                new TeleOpSlideControl(slides, gamepad1),
+                new TeleOpCarouselControl(carousel, gamepad2),
+                new TeleOpIntakeControl(intake, gamepad2)
+        );
+    }
+}
