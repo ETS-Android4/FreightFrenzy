f15b2efc59218cf7d039bdeb66cdcc056e7e6219
Blake Sanders <54512462+BenRodkin@users.noreply.github.com>
2021-11-07 17:27:32 -0500

    Fix to known bug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/auto/SlidesByEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/auto/SlidesByEncoder.java
@@ -82,6 +82,6 @@ public class SlidesByEncoder implements Command {
 
     @Override
     public boolean isCompleted() {
-        return !slides.isBusy() || timer.seconds() > timeout;
+        return (!slides.isBusy() && timer.seconds() > 0.5) || timer.seconds() > timeout;
     }
 }
