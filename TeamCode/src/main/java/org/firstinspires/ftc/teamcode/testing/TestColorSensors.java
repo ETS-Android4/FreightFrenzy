42d996860a695005bb6cf2d2e422b11be4bfb4a0
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2021-12-08 21:23:58 -0500

    Implemented Data Snooper into a TeleOp program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestColorSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestColorSensors.java
@@ -7,6 +7,8 @@ import com.disnodeteam.dogecommander.DogeOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpIntakeColorSensor;
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpLineColorSensor;
 import org.firstinspires.ftc.teamcode.robot.subsystems.IntakeColorSensor;
 import org.firstinspires.ftc.teamcode.robot.subsystems.LineColorSensor;
 
@@ -22,31 +24,39 @@ public class TestColorSensors extends LinearOpMode implements DogeOpMode {
         LineColorSensor line = new LineColorSensor(hardwareMap);
         IntakeColorSensor intake = new IntakeColorSensor(hardwareMap);
 
-        line.initHardware();
-        intake.initHardware();
+        commander.registerSubsystem(line);
+        commander.registerSubsystem(intake);
 
-        while (!isStarted() && !isStopRequested()) {
-            line.periodic();
-            intake.periodic();
-            double[] val = line.getColorLeft().val;
-            telemetry.addLine(String.format("l: [r,g,b] = [%.2f, %.2f, %.2f]", val[0], val[1], val[2]));
-            val = line.normalizeByMax(line.getColorLeft(), 255);
-            telemetry.addLine(String.format("|l|: [r,g,b] = [%.2f, %.2f, %.2f]", val[0], val[1], val[2]));
-            val = line.getColorRight().val;
-            telemetry.addLine(String.format("r: [r,g,b] = [%.2f, %.2f, %.2f]", val[0], val[1], val[2]));
-            val = line.normalizeByMax(line.getColorRight(), 255);
-            telemetry.addLine(String.format("|r|: [r,g,b] = [%.2f, %.2f, %.2f]", val[0], val[1], val[2]));
-            telemetry.addLine();
-
-            val = intake.getColor().val;
-            telemetry.addLine(String.format("i: [r,g,b] = [%.2f, %.2f, %.2f]", val[0], val[1], val[2]));
-
-
-
-            telemetry.update();
-        }
+//        line.initHardware();
+//        intake.initHardware();
+//
+//        while (!isStarted() && !isStopRequested()) {
+//            line.periodic();
+//            intake.periodic();
+//            double[] val = line.getColorLeft().val;
+//            telemetry.addLine(String.format("l: [r,g,b] = [%.2f, %.2f, %.2f]", val[0], val[1], val[2]));
+//            val = line.normalizeByMax(line.getColorLeft(), 255);
+//            telemetry.addLine(String.format("|l|: [r,g,b] = [%.2f, %.2f, %.2f]", val[0], val[1], val[2]));
+//            val = line.getColorRight().val;
+//            telemetry.addLine(String.format("r: [r,g,b] = [%.2f, %.2f, %.2f]", val[0], val[1], val[2]));
+//            val = line.normalizeByMax(line.getColorRight(), 255);
+//            telemetry.addLine(String.format("|r|: [r,g,b] = [%.2f, %.2f, %.2f]", val[0], val[1], val[2]));
+//            telemetry.addLine();
+//
+//            val = intake.getColor().val;
+//            telemetry.addLine(String.format("i: [r,g,b] = [%.2f, %.2f, %.2f]", val[0], val[1], val[2]));
+//
+//
+//
+//            telemetry.update();
+//        }
 
         commander.init();
         waitForStart();
+
+        commander.runCommandsParallel(
+                new TeleOpLineColorSensor(line, true),
+                new TeleOpIntakeColorSensor(intake, true)
+        );
     }
 }
