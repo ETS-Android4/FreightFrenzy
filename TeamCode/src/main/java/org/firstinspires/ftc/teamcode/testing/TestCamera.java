5ae2f397fc3374ca21831489a9ec07b2e8c0daad
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2021-12-09 20:47:16 -0500

    Sketched basic plan for updated auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestCamera.java
@@ -10,6 +10,7 @@ import org.checkerframework.checker.units.qual.C;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.AutoCameraControl;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Camera;
 import org.firstinspires.ftc.teamcode.swampbots_util.DuckPatternPipeline;
+import org.firstinspires.ftc.teamcode.swampbots_util.DuckPlacement;
 
 @Autonomous(name = "test cam", group = "testing")
 public class TestCamera extends LinearOpMode implements DogeOpMode {
@@ -36,8 +37,70 @@ public class TestCamera extends LinearOpMode implements DogeOpMode {
         waitForStart();
 
         telemetry.addLine("ran");
+        telemetry.addData("placement", cam.getPlacement());
         telemetry.update();
         sleep(3000);
 
+        switch (choosePlacement(cam)) {
+            case LEFT:
+                runPathBottom();
+                break;
+            case CENTER:
+                runPathMiddle();
+                break;
+            case RIGHT:
+                runPathTop();
+                break;
+            case UNKNOWN: //Run most point path if something goes wrong
+                runPathTop();
+                break;
+        }
+
+    }
+
+    private void runCommonPathBeforeSplit() {
+        telemetry.addLine("common path 1");
+        telemetry.update();
+
+
+    }
+
+    private void runPathBottom() {
+        runCommonPathBeforeSplit();
+        telemetry.addLine("bottom path");
+        telemetry.update();
+
+        runCommonPathAfterSplit();
+    }
+
+    private void runPathMiddle() {
+        runCommonPathBeforeSplit();
+        telemetry.addLine("middle path");
+
+
+        runCommonPathAfterSplit();
+    }
+
+    private void runPathTop() {
+        runCommonPathBeforeSplit();
+        telemetry.addLine("top path");
+
+
+        runCommonPathAfterSplit();
+    }
+
+    private void runCommonPathAfterSplit() {
+        telemetry.addLine("common path 2");
+
+    }
+
+    private DuckPlacement choosePlacement(AutoCameraControl cam) {
+        DuckPlacement placement = cam.getPlacement();
+        if(!placement.name().equals(DuckPlacement.UNKNOWN.name())) {
+            cam.periodic();
+            return choosePlacement(cam);
+        }
+
+        return placement;
     }
 }
