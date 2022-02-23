3b072b6e589bc13ebd744a9df7e1dad8787eee8f
10neb01 <Benrodkin042@gmail.com>
2022-02-23 18:50:23 -0500

    Test program for Carousel color sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestCarouselColorSensor.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.disnodeteam.dogecommander.DogeCommander;
+import com.disnodeteam.dogecommander.DogeOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpCarouselControl;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Carousel;
+import org.firstinspires.ftc.teamcode.robot.subsystems.CarouselColorSensor;
+
+@TeleOp(name = "Test Carousel Color", group = "testing")
+public class TestCarouselColorSensor extends LinearOpMode implements DogeOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DogeCommander commander = new DogeCommander(this);
+
+        CarouselColorSensor carouselColorSensor = new CarouselColorSensor(hardwareMap, telemetry);
+        Carousel carousel = new Carousel(hardwareMap);
+        // Subsystem sub = new Subsystem()
+
+        commander.registerSubsystem(carouselColorSensor);
+        commander.registerSubsystem(carousel);
+        // commander.registerSubsystem(sub);
+
+        commander.init();
+        waitForStart();
+
+        commander.runCommandsParallel(
+                new TeleOpCarouselControl(carousel, gamepad1)
+                // new CommandToTest(sub...)
+        );
+    }
+}
