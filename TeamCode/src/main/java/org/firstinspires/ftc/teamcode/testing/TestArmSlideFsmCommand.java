73a35c097ff0e42315853ba8481bd72c9458544d
10neb01 <Benrodkin042@gmail.com>
2022-02-24 20:45:20 -0500

    Updated test program to use the new Color Sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestArmSlideFsmCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestArmSlideFsmCommand.java
@@ -8,7 +8,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpArmSlideControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpArmSlideKickerControl;
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpArmSlideKickerControlPlus;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.robot.subsystems.IntakeColorSensor;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Kicker;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Slides;
 
@@ -22,16 +24,18 @@ public class TestArmSlideFsmCommand extends LinearOpMode implements DogeOpMode {
         Arm arm = new Arm(hardwareMap);
         Slides slides = new Slides(hardwareMap);
         Kicker kicker = new Kicker(hardwareMap);
+        IntakeColorSensor intakeColorSensor = new IntakeColorSensor(hardwareMap);
 
         commander.registerSubsystem(arm);
         commander.registerSubsystem(slides);
         commander.registerSubsystem(kicker);
+        commander.registerSubsystem(intakeColorSensor);
 
         commander.init();
         waitForStart();
 
         commander.runCommandsParallel(
-                 new TeleOpArmSlideKickerControl(arm, slides, kicker, gamepad1, telemetry)
+                 new TeleOpArmSlideKickerControlPlus(arm, slides, kicker, intakeColorSensor, gamepad1, telemetry)
         );
     }
 }
