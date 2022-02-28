3f0a32b5ced044d14ca2100b1e5f00f16de69a5d
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2022-02-27 22:44:00 -0500

    Updated Capstone for it's new design

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Cap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Cap.java
@@ -1,16 +1,34 @@
 package org.firstinspires.ftc.teamcode.robot.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.disnodeteam.dogecommander.Subsystem;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 public class Cap implements Subsystem {
     private HardwareMap hardwareMap;
-    private CRServo cap;
+    private Servo cap;
 
-    private double power;
-    private double prevPower = 0.0;
+    private POSITIONS position;
+
+    public static double testPos = -1;
+
+    public enum POSITIONS {
+        IN,
+        OUT;
+
+        public double getPosition() {
+            switch (this) {
+                case IN:
+                    return 0.0;
+                case OUT:
+                    return 0.5;
+                default:
+                    return IN.getPosition();
+            }
+        }
+    }
 
     public Cap(HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
@@ -18,38 +36,38 @@ public class Cap implements Subsystem {
 
     @Override
     public void initHardware() {
-        cap = hardwareMap.get(CRServo.class, "cap");
+        cap = hardwareMap.get(Servo.class, "cap");
+        cap.setDirection(Servo.Direction.FORWARD);
 
-        cap.setDirection(DcMotorSimple.Direction.FORWARD);
+        position = POSITIONS.IN;
 
-        power = 0.0;
-
-        cap.setPower(power);
     }
 
     @Override
     public void periodic() {
-        cap.setPower(power);
+        double pos = position.getPosition();
+
+        // Easy position testing setup using FtcDashboard
+        if(testPos != -1.0) {
+            pos = testPos;
+        }
+
+        cap.setPosition(pos);
     }
 
-    public void setPower(double power) {
-        this.power = power;
+    public void setPosition(POSITIONS position) {
+        this.position = position;
     }
 
-    public void setDirection(DcMotorSimple.Direction direction) {
+    public void setDirection(Servo.Direction direction) {
         cap.setDirection(direction);
     }
 
-    public double getPower() {
-        return power;
+    public POSITIONS getPosition() {
+        return position;
     }
 
-    public DcMotorSimple.Direction getDirection() {
+    public Servo.Direction getDirection() {
         return cap.getDirection();
-    }
-
-    public void reverse() {
-        if(getDirection() == DcMotorSimple.Direction.FORWARD) setDirection(DcMotorSimple.Direction.REVERSE);
-        else setDirection(DcMotorSimple.Direction.FORWARD);
     }
 }
