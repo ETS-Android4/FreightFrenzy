550fa3c0005e11d89cb0cf3e78c8b93363cd1bd5
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2022-03-02 23:00:19 -0500

    Implemented Camera into Warehouse Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWarehouseBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWarehouseBlue.java
@@ -5,10 +5,12 @@ import com.disnodeteam.dogecommander.DogeOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.checkerframework.checker.units.qual.A;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.ActionAfterDelay;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.ActionAfterStatement;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.ActionUntilStatement;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.ArmSetState;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.AutoCameraControl;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.DriveByEncoder;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.DriveByTimer;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.KickerSetState;
@@ -16,14 +18,17 @@ import org.firstinspires.ftc.teamcode.robot.commands.auto.RunCarouselForTime;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.SlidesByEncoder;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.TurnByGyroPID;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Camera;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Carousel;
 import org.firstinspires.ftc.teamcode.robot.subsystems.CarouselColorSensor;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Kicker;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Slides;
+import org.firstinspires.ftc.teamcode.swampbots_util.DuckPlacement;
 import org.firstinspires.ftc.teamcode.swampbots_util.SwampbotsUtil;
 
+import java.util.Date;
 import java.util.function.BooleanSupplier;
 
 @Autonomous(name = "Warehouse Blue", group = "finalized")
@@ -35,8 +40,11 @@ public class AutoWarehouseBlue extends LinearOpMode implements DogeOpMode {
     private Slides slides;
     private Kicker kicker;
     private Intake intake;
+    private AutoCameraControl cam;
 
     private SwampbotsUtil util = new SwampbotsUtil();
+
+    public static double CAM_OVERRIDE = -1;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -47,6 +55,7 @@ public class AutoWarehouseBlue extends LinearOpMode implements DogeOpMode {
         slides = new Slides(hardwareMap);
         kicker = new Kicker(hardwareMap);
         intake = new Intake(hardwareMap);
+        cam = new AutoCameraControl(new Camera(hardwareMap, telemetry), gamepad1, gamepad2, telemetry);
 
         commander.registerSubsystem(drive);
         commander.registerSubsystem(arm);
@@ -54,8 +63,16 @@ public class AutoWarehouseBlue extends LinearOpMode implements DogeOpMode {
         commander.registerSubsystem(kicker);
         commander.registerSubsystem(intake);
 
-        telemetry.addLine("3");
-        telemetry.update();
+        // Wait for cam to initialize
+        while (!cam.getCamera().hasInitialized && !opModeIsActive() && !isStopRequested());
+
+        while (!isStopRequested() && !opModeIsActive()) {
+            telemetry.addData("pre-init @", new Date().getTime());
+            cam.periodic();
+            if(isStopRequested()) {
+                cam.getCamera().stop();
+            }
+        }
 
         commander.init();
 
@@ -65,15 +82,119 @@ public class AutoWarehouseBlue extends LinearOpMode implements DogeOpMode {
         telemetry.addLine("Started");
         telemetry.update();
 
-//        commander.runCommand(new ArmSetState(arm, Arm.POSITION.MIDDLE));
-//        telemetry.addData("pos", arm.getTargetPos());
-//        telemetry.update();
-//sleep(5000);
+        if(CAM_OVERRIDE == -1) {
+            switch (choosePlacement(cam)) {
+                case LEFT:
+                    runPathTop();
+                    break;
+                case CENTER:
+                    runPathMiddle();
+                    break;
+                case RIGHT:
+                    runPathBottom();
+                    break;
+                case UNKNOWN: //Run most point path if something goes wrong
+                    runPathTop();
+                    break;
+            }
+        } else {
+            switch (((int) CAM_OVERRIDE)) {
+                case 0:
+                    runPathBottom();
+                    break;
+                case 1:
+                    runPathMiddle();
+                    break;
+                case 2:
+                    runPathTop();
+                    break;
+                case 4:
+                    runCommonPathBeforeSplit();
+                    break;
+                case 5:
+                    runCommonPathAfterSplit();
+                    break;
+            }
+        }
+
+        commander.stop();
+    }
+
+    private void runCommonPathBeforeSplit() {
+        telemetry.addLine("common path 1");
+        telemetry.update();
+        cam.getCamera().stop();
+        sleep(1000);
+
         commander.runCommand(new ArmSetState(arm, Arm.POSITION.MIDDLE));
         commander.runCommand(new DriveByEncoder(drive, SwampbotsUtil.inchToCount(-4), 0, 0.3, telemetry));
         sleep(300);
         commander.runCommand(new TurnByGyroPID(drive, telemetry, -25, 0.3));
         sleep(300);
+
+    }
+
+    private void runPathBottom() {
+        runCommonPathBeforeSplit();
+        telemetry.addLine("bottom path");
+        telemetry.update();
+        sleep(1000);
+
+        commander.runCommand(new DriveByEncoder(drive, SwampbotsUtil.inchToCount(-4), -25, 0.3, telemetry));
+        commander.runCommandsParallel(
+//                new ArmSetState(arm, Arm.POSITION.MIDDLE),
+                new ActionAfterDelay(
+                        new SlidesByEncoder(slides, Slides.TARGETS.BOTTOM_HUB, 0.7),
+                        0.5
+                ),
+                new ActionAfterStatement(
+                        new ArmSetState(arm, Arm.POSITION.LOW_HUB),
+                        new BooleanSupplier() {
+                            @Override
+                            public boolean getAsBoolean() {
+                                return util.isCloseEnough(slides.getCurrentPos(), Slides.TARGETS.MIDDLE.getTargets() - 50, 10);
+                            }
+                        }
+                ));
+        sleep(500);
+
+        runCommonPathAfterSplit();
+    }
+
+    private void runPathMiddle() {
+        runCommonPathBeforeSplit();
+        telemetry.addLine("middle path");
+        telemetry.update();
+        sleep(1000);
+
+        commander.runCommand(new DriveByEncoder(drive, SwampbotsUtil.inchToCount(-4), -25, 0.3, telemetry));
+        commander.runCommandsParallel(
+//                new ArmSetState(arm, Arm.POSITION.MIDDLE),
+                new ActionAfterDelay(
+                        new SlidesByEncoder(slides, Slides.TARGETS.OUT, 0.7),
+                        0.5
+                ),
+                new ActionAfterStatement(
+                        new ArmSetState(arm, Arm.POSITION.MIDDLE),
+                        new BooleanSupplier() {
+                            @Override
+                            public boolean getAsBoolean() {
+                                return util.isCloseEnough(slides.getCurrentPos(), Slides.TARGETS.MIDDLE.getTargets() - 50, 10);
+                            }
+                        }
+                ));
+        sleep(500);
+
+
+        runCommonPathAfterSplit();
+    }
+
+    private void runPathTop() {
+        runCommonPathBeforeSplit();
+        telemetry.addLine("top path");
+        telemetry.update();
+        sleep(1000);
+
         commander.runCommand(new DriveByEncoder(drive, SwampbotsUtil.inchToCount(-4), -25, 0.3, telemetry));
         commander.runCommandsParallel(
 //                new ArmSetState(arm, Arm.POSITION.MIDDLE),
@@ -91,6 +212,15 @@ public class AutoWarehouseBlue extends LinearOpMode implements DogeOpMode {
                         }
                 ));
         sleep(500);
+
+        runCommonPathAfterSplit();
+    }
+
+    private void runCommonPathAfterSplit() {
+        telemetry.addLine("common path 2");
+        telemetry.update();
+        sleep(1000);
+
         commander.runCommand(new KickerSetState(kicker, Kicker.POSITION.CLOSE));
         sleep(300);
         commander.runCommand(new ArmSetState(arm, Arm.POSITION.MIDDLE));
@@ -112,8 +242,21 @@ public class AutoWarehouseBlue extends LinearOpMode implements DogeOpMode {
         commander.runCommand(new DriveByEncoder(drive, SwampbotsUtil.inchToCount(6), -25, 0.3, telemetry));
         commander.runCommand(new DriveByEncoder(drive, SwampbotsUtil.inchToCount(12), -80, 0.3, telemetry));
         commander.runCommand(new DriveByEncoder(drive, SwampbotsUtil.inchToCount(30), -90, 0.3, telemetry));
+    }
 
-        commander.stop();
+    private DuckPlacement choosePlacement(AutoCameraControl cam) {
+        DuckPlacement placement = cam.getPlacement();
+        if(placement.name().equals(DuckPlacement.UNKNOWN.name())) {
+            telemetry.addData("choosing at", new Date().getTime());
+            cam.periodic();
+            if(isStopRequested()) {
+                cam.getCamera().stop();
+                return null;
+            }
+            return choosePlacement(cam);
+        }
+
+        return placement;
     }
 
 }
