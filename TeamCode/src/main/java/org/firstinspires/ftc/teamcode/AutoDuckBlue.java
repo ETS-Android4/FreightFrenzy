de2dcd15f3697bb50e27e5a31d8583d4abbda8b3
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2022-03-02 23:05:47 -0500

    Added Camera into Duck Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDuckBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDuckBlue.java
@@ -1,5 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.teamcode.swampbots_util.DuckPlacement.CENTER;
+import static org.firstinspires.ftc.teamcode.swampbots_util.DuckPlacement.LEFT;
+import static org.firstinspires.ftc.teamcode.swampbots_util.DuckPlacement.RIGHT;
+import static org.firstinspires.ftc.teamcode.swampbots_util.DuckPlacement.UNKNOWN;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.acmerobotics.dashboard.config.Config;
 import com.disnodeteam.dogecommander.DogeCommander;
 import com.disnodeteam.dogecommander.DogeOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -9,6 +18,7 @@ import org.firstinspires.ftc.teamcode.robot.commands.auto.ActionAfterDelay;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.ActionAfterStatement;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.ActionUntilStatement;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.ArmSetState;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.AutoCameraControl;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.DriveByEncoder;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.DriveByTimer;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.KickerSetState;
@@ -16,15 +26,19 @@ import org.firstinspires.ftc.teamcode.robot.commands.auto.RunCarouselForTime;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.SlidesByEncoder;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.TurnByGyroPID;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Camera;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Carousel;
 import org.firstinspires.ftc.teamcode.robot.subsystems.CarouselColorSensor;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Kicker;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Slides;
+import org.firstinspires.ftc.teamcode.swampbots_util.DuckPlacement;
 import org.firstinspires.ftc.teamcode.swampbots_util.SwampbotsUtil;
 
+import java.util.Date;
 import java.util.function.BooleanSupplier;
 
+@Config
 @Autonomous(name = "Duck Blue", group = "finalized")
 public class AutoDuckBlue extends LinearOpMode implements DogeOpMode {
     private DogeCommander commander;
@@ -35,8 +49,11 @@ public class AutoDuckBlue extends LinearOpMode implements DogeOpMode {
     private Arm arm;
     private Slides slides;
     private Kicker kicker;
+    private AutoCameraControl cam;
 
     private SwampbotsUtil util = new SwampbotsUtil();
+
+    public static double CAM_OVERRIDE = -1;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -48,6 +65,8 @@ public class AutoDuckBlue extends LinearOpMode implements DogeOpMode {
         arm = new Arm(hardwareMap);
         slides = new Slides(hardwareMap);
         kicker = new Kicker(hardwareMap);
+        cam = new AutoCameraControl(new Camera(hardwareMap, telemetry), gamepad1, gamepad2, telemetry);
+
 
         commander.registerSubsystem(drive);
         commander.registerSubsystem(carousel);
@@ -56,9 +75,17 @@ public class AutoDuckBlue extends LinearOpMode implements DogeOpMode {
         commander.registerSubsystem(slides);
         commander.registerSubsystem(kicker);
 
-        telemetry.addLine("3");
-        telemetry.update();
+        // Wait for cam to initialize
+        while (!cam.getCamera().hasInitialized && !opModeIsActive() && !isStopRequested());
 
+        while (!isStopRequested() && !opModeIsActive()) {
+            telemetry.addData("pre-init @", new Date().getTime());
+            cam.periodic();
+            if(isStopRequested()) {
+                cam.getCamera().stop();
+            }
+        }
+        
         commander.init();
 
         telemetry.addLine("Ready!");
@@ -67,50 +94,85 @@ public class AutoDuckBlue extends LinearOpMode implements DogeOpMode {
         telemetry.addLine("Started");
         telemetry.update();
 
+        if(CAM_OVERRIDE == -1) {
+            switch (choosePlacement(cam)) {
+                case LEFT:
+                    runPathTop();
+                    break;
+                case CENTER:
+                    runPathMiddle();
+                    break;
+                case RIGHT:
+                    runPathBottom();
+                    break;
+                case UNKNOWN: //Run most point path if something goes wrong
+                    runPathTop();
+                    break;
+            }
+        } else {
+            switch (((int) CAM_OVERRIDE)) {
+                case 0:
+                    runPathBottom();
+                    break;
+                case 1:
+                    runPathMiddle();
+                    break;
+                case 2:
+                    runPathTop();
+                    break;
+                case 4:
+                    runCommonPathBeforeSplit();
+                    break;
+                case 5:
+                    runCommonPathAfterSplit();
+                    break;
+            }
+        }
+        
 //        commander.runCommand(new ArmSetState(arm, Arm.POSITION.MIDDLE));
 //        telemetry.addData("pos", arm.getTargetPos());
 //        telemetry.update();
 //sleep(5000);
-        runCommonPathBeforeSplit();
-
-        commander.runCommandsParallel(
-//                new ArmSetState(arm, Arm.POSITION.MIDDLE),
-                new ActionAfterDelay(
-                        new SlidesByEncoder(slides, Slides.TARGETS.OUT, 0.7),
-                        0.5
-                ),
-                new ActionAfterStatement(
-                        new ArmSetState(arm, Arm.POSITION.DEPOSIT),
-                        new BooleanSupplier() {
-                            @Override
-                            public boolean getAsBoolean() {
-                                return util.isCloseEnough(slides.getCurrentPos(), Slides.TARGETS.MIDDLE.getTargets() - 50, 10);
-                            }
-                        }
-                ));
-        sleep(500);
-        commander.runCommand(new KickerSetState(kicker, Kicker.POSITION.CLOSE));
-        sleep(300);
-        commander.runCommand(new ArmSetState(arm, Arm.POSITION.MIDDLE));
-        sleep(1000);
-        commander.runCommandsParallel(
-                new SlidesByEncoder(slides, Slides.TARGETS.IN, 0.5)//,
+//        runCommonPathBeforeSplit();
+//
+//        commander.runCommandsParallel(
+////                new ArmSetState(arm, Arm.POSITION.MIDDLE),
+//                new ActionAfterDelay(
+//                        new SlidesByEncoder(slides, Slides.TARGETS.OUT, 0.7),
+//                        0.5
+//                ),
 //                new ActionAfterStatement(
-//                        new ArmSetState(arm, Arm.POSITION.INTAKE),
+//                        new ArmSetState(arm, Arm.POSITION.DEPOSIT),
 //                        new BooleanSupplier() {
 //                            @Override
 //                            public boolean getAsBoolean() {
-//                                return util.isCloseEnough(slides.getCurrentPos(), Slides.TARGETS.MIDDLE.getTargets(), 50);
+//                                return util.isCloseEnough(slides.getCurrentPos(), Slides.TARGETS.MIDDLE.getTargets() - 50, 10);
 //                            }
 //                        }
-//                )
-        );
-        sleep(1000);
-
-        commander.runCommand(new DriveByEncoder(drive, SwampbotsUtil.inchToCount(25.0), 50, 0.3, telemetry));
-        sleep(1000);
-
-        runCommonPathAfterSplit();
+//                ));
+//        sleep(500);
+//        commander.runCommand(new KickerSetState(kicker, Kicker.POSITION.CLOSE));
+//        sleep(300);
+//        commander.runCommand(new ArmSetState(arm, Arm.POSITION.MIDDLE));
+//        sleep(1000);
+//        commander.runCommandsParallel(
+//                new SlidesByEncoder(slides, Slides.TARGETS.IN, 0.5)//,
+////                new ActionAfterStatement(
+////                        new ArmSetState(arm, Arm.POSITION.INTAKE),
+////                        new BooleanSupplier() {
+////                            @Override
+////                            public boolean getAsBoolean() {
+////                                return util.isCloseEnough(slides.getCurrentPos(), Slides.TARGETS.MIDDLE.getTargets(), 50);
+////                            }
+////                        }
+////                )
+//        );
+//        sleep(1000);
+//
+//        commander.runCommand(new DriveByEncoder(drive, SwampbotsUtil.inchToCount(25.0), 50, 0.3, telemetry));
+//        sleep(1000);
+//
+//        runCommonPathAfterSplit();
 
         commander.stop();
     }
@@ -123,7 +185,7 @@ public class AutoDuckBlue extends LinearOpMode implements DogeOpMode {
         sleep(300);
     }
 
-    public void runLowPath() {
+    public void runPathBottom() {
         commander.runCommandsParallel(
                 new ArmSetState(arm, Arm.POSITION.MIDDLE),
                 new ActionAfterDelay(
@@ -162,7 +224,7 @@ public class AutoDuckBlue extends LinearOpMode implements DogeOpMode {
         sleep(1000);
     }
 
-    public void runMiddlePath() {
+    public void runPathMiddle() {
         commander.runCommandsParallel(
                 new ArmSetState(arm, Arm.POSITION.MIDDLE),
                 new ActionAfterDelay(
@@ -201,7 +263,7 @@ public class AutoDuckBlue extends LinearOpMode implements DogeOpMode {
         sleep(1000);
     }
 
-    public void runHighPath() {
+    public void runPathTop() {
         commander.runCommandsParallel(
                 new ArmSetState(arm, Arm.POSITION.MIDDLE),
                 new ActionAfterDelay(
@@ -262,4 +324,19 @@ public class AutoDuckBlue extends LinearOpMode implements DogeOpMode {
 
     }
 
+    @Nullable
+    private DuckPlacement choosePlacement(@NonNull AutoCameraControl cam) {
+        DuckPlacement placement = cam.getPlacement();
+        if(placement.name().equals(DuckPlacement.UNKNOWN.name())) {
+            telemetry.addData("choosing at", new Date().getTime());
+            cam.periodic();
+            if(isStopRequested()) {
+                cam.getCamera().stop();
+                return null;
+            }
+            return choosePlacement(cam);
+        }
+
+        return placement;
+    }
 }
